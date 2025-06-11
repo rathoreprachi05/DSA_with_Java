2d461bf321fa14a37f8a0f765e1fdee5f2ba2788
Prachi Rathore <rathoreprachi266@gmail.com>
2025-06-11 22:46:17 +0530

    updated

--- Day2/NoofDigits.java
+++ Day2/NoofDigits.java
@@ -1,16 +1,26 @@
 public class NoofDigits{
+
+    // Iteratively
     public int noofdigits(int n){
         int count = 0;
-        while(n>=0){
+        if(n == 0) return 1;
+        while(n>0){
             n /= 10;
             count++;
         }
-        return count++;
+        return count;
+    }
+
+    // recursively
+    public int noofdigitsrec(int n){
+        if(n >=0 && n <= 9) return 1;
+        return (1 + noofdigitsrec(n/10));
     }
 
     public static void main(String[] args){
         NoofDigits obj = new NoofDigits();
 
         System.out.println(obj.noofdigits(121));
+        System.out.println(obj.noofdigitsrec(1217));
     }
 }
