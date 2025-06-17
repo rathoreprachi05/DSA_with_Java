86b09dbda380990c970b8fa785b6e4a708ba1876
Prachi Rathore <rathoreprachi266@gmail.com>
2025-06-17 17:17:45 +0530

    Static Inner Class

--- Day6/NestedClasses.java
+++ Day6/NestedClasses.java
@@ -9,10 +9,24 @@ class Outer1{
     }
 }
 
+class Outer2{
+    static int x = 100;
+
+    static class Inner2{
+        void show(){
+            x++;
+            System.out.println(x);
+        }
+    }
+}
+
 class NestedClasses{
     public static void main(String[] args){
-        Outer1 obj = new Outer1();
-        Outer1.Inner1 inner = obj.new Inner1();
+        Outer1 obj1 = new Outer1();
+        Outer1.Inner1 inner = obj1.new Inner1();
         inner.show();
+
+        Outer2.Inner2 obj2 = new Outer2.Inner2();
+        obj2.show();
     }
 }
