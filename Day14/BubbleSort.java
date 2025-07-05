bbfc4cdb49df0954462b46a4708067b90903d0f0
Prachi Rathore <rathoreprachi266@gmail.com>
2025-07-05 23:10:24 +0530

    Bubble Sort

---
+++ Day14/BubbleSort.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class BubbleSort {
+    int[] bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BubbleSort sorter = new BubbleSort();
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] sortedArr = sorter.bubbleSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
+
+
