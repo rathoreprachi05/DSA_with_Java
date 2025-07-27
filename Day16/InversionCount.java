cef9a04aaeb4e3008a3974606d896bf305c71bf5
Prachi Rathore <rathoreprachi266@gmail.com>
2025-07-27 21:52:01 +0530

    Brute force approach of finding inversion count of an array

---
+++ Day16/InversionCount.java
@@ -0,0 +1,23 @@
+public class InversionCount{
+
+    // Brute Force approach ---> Time Complexity : O(n^2)
+
+    int inversionCount(int arr[]){
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]) count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {4,3,2,1};
+        InversionCount obj = new InversionCount();
+        System.out.println(obj.inversionCount(arr));
+    }
+}
+
+
+
