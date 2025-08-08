fb9440c776a538795c4fae8f80e8857a151eb40b
Prachi Rathore <rathoreprachi266@gmail.com>
2025-08-08 19:20:18 +0530

    Last Occurence of an el in an int arr using recursion

---
+++ Day17/RecLastOcc.java
@@ -0,0 +1,28 @@
+class RecLastOcc{
+    int recLastOcc(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1; 
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            int rightResult = recLastOcc(arr, target, mid + 1, end);
+            return (rightResult != -1) ? rightResult : mid;
+        }
+        else if (arr[mid] > target) {
+            return recLastOcc(arr, target, start, mid - 1);
+        }
+        else {
+            return recLastOcc(arr, target, mid + 1, end);
+        }
+    }
+    public static void main(String[] args) {
+        RecLastOcc obj = new RecLastOcc();
+        int[] arr = {1, 2, 2, 2, 3};
+        
+        System.out.println(obj.recLastOcc(arr, 2, 0, arr.length - 1)); // expect 3
+        System.out.println(obj.recLastOcc(arr, 1, 0, arr.length - 1)); // expect 0
+        System.out.println(obj.recLastOcc(arr, 4, 0, arr.length - 1)); // expect -1
+    }
+}
