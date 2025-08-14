e662e554b0add826de641100af275adbfba47df5
Prachi Rathore <rathoreprachi266@gmail.com>
2025-08-14 22:38:22 +0530

    Searching for an el in a roatated array at a random index - brute force ---> using linear search

---
+++ Day18/LinearSortedRotated.java
@@ -0,0 +1,14 @@
+class LinearSortedRotated{
+    boolean searchInRotatedArr(int[] arr, int target){
+        for(int i=0; i<=arr.length-1; i++){
+            if(arr[i] == target) return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        LinearSortedRotated lsr = new LinearSortedRotated();
+        int[] arr = {3,4,5,1,2};
+        int target = 5;
+        System.out.println(lsr.searchInRotatedArr(arr, target));
+    }
+}
