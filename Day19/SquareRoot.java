152bc14d0c363e1778002ccfffba14ca8a78be1e
rathoreprachi05 <rathoreprachi266@gmail.com>
2025-11-12 23:47:59 +0530

    A test commit

---
+++ Day19/SquareRoot.java
@@ -0,0 +1,13 @@
+//  LeetCode Problem Link: https://leetcode.com/problems/sqrtx/description/
+
+/*Question: Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well. You must not use any built-in exponent function or operator (such as pow(x, 0.5) or x ** 0.5).
+
+Examples: 
+
+*) Input: x = 4 → Output: 2
+
+*) Input: x = 8 → Output: 2 (since the true sqrt ≈ 2.828…, floor is 2)
+*/
+
+
+This is a test.
