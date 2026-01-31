c2cdac56859f9689f3a89baf72add70079eda9b9
Prachi Rathore <148968395+rathoreprachi05@users.noreply.github.com>
2026-01-31 22:28:22 +0530

    Update InsertionSort.java with more problem links
    Added additional relevant problem links for Insertion Sort.

--- Day14/InsertionSort.java
+++ Day14/InsertionSort.java
@@ -1,6 +1,10 @@
 /* Relevant Problem Links:
     1. https://www.hackerrank.com/challenges/insertionsort1/problem
     2. https://leetcode.com/problems/insertion-sort-list/description/
+    3. https://www.geeksforgeeks.org/problems/insertion-sort/1
+    4. https://www.hackerrank.com/challenges/insertionsort2/problem
+    5. https://leetcode.com/problems/sort-an-array/description/
+    6. https://www.codechef.com/problems/TSORT
 */
 
 class InsertionSort{
